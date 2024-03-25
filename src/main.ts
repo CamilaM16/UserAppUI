7554b72ef146f96cec36fde618cd0b147ff584ca
Camila.Malaga <Camila.Malaga@jalasoft.com>
2024-03-25 16:00:14 -0400

    [Set Up]: Add kendoUI and Knockout with ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-function hello(compiler: string) {
-    console.log(`Hello from ${compiler}`);
-  }
-  hello("TypeScript");
+import { sayHello } from "./greet";
+function showHello(divName: string, name: string) {
+  const elt = document.getElementById(divName);
+  elt.innerText = sayHello(name);
+}
+showHello("greeting", "TypeScript");
